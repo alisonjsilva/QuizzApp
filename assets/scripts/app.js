c9fbece848cabb8710f4a713631181eb3226bf02
alisonjsilva <alisonjsilva@hotmail.com>
2017-01-20 01:10:48 +0000

    Adicionado navegação Adicionadas páginas Home e Código

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,31 +1,28 @@
-if (typeof(Storage) !== "undefined") {
-  localStorage.setItem('name', 'Alison');
-  document.getElementById('name').innerHTML = localStorage.getItem('name');
+// when app app is load
+$('#app').load('home.html', function(){
+  console.log('home');
+}).hide().fadeIn();
 
-  var people =
-  [{
-    name:'Alison',
-    lastName:'Silva'
-  },
-  {
-    name:'Roberta',
-    lastName:'Mercês Silva'
-  },
-  {
-    name: 'Sara',
-    lastName:'Silva'
+// navigation function
+var navigation = (function() {
+
+  var nav = function () {
+
+    $(document).on('click', '.navTo', function() {
+
+      var page = $(this).data('page');
+
+      $('#app').load(page, function(){
+
+      })
+      .hide()
+      .fadeIn();
+    });
+
+  };
+
+  return {
+    page: nav()
   }
-];
 
-for (person of people) {
-  console.log('# ' + person.name);
-};
-  localStorage.setItem('people', people);
-
-  console.log(localStorage.getItem('people'));
-
-} else {
-  console.log('dont have local Storage')
-}
-
-// http://www.favicon-generator.org/
+})();
