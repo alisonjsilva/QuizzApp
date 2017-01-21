2a78b3edca7fabf8a7b0106843f07ed79e4b0974
alisonjsilva <alisonjsilva@hotmail.com>
2017-01-21 00:35:26 +0000

    Added callback functions in route

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,58 @@
-// when app app is load
-$('#app').load('home.html', function(){
-  console.log('home');
-}).hide().fadeIn();
+
 
 // navigation function
 var navigation = (function() {
+  "use strict"
 
+  // handler the click and call load
   var nav = function () {
 
     $(document).on('click', '.navTo', function() {
 
       var page = $(this).data('page');
+      var _callback = $(this).data('callback');
 
-      $('#app').load(page, function(){
+      load(page, _callback);
 
-      })
-      .hide()
-      .fadeIn();
     });
 
   };
 
+  // call load function
+  var load = function (page, callback){
+    $('#app').load(page, function(){
+
+      if(typeof callback === 'function'){
+        callback();
+      } else if (typeof callback === 'string') {
+
+        var _function = window[callback];
+        if(typeof _function === 'function'){
+            _function();
+
+        }
+      }
+
+    })
+    .hide()
+    .fadeIn();
+  };
+
   return {
-    page: nav()
+    page: nav(),
+    load: load
   }
 
 })();
+
+navigation.load('home.html', function () {
+
+});
+
+function home() {
+  console.log('this is home');
+}
+
+function codigo() {
+  console.log('this is codigo page');
+}
